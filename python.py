357f58e85fd7d6073a51660941ec5e850588c4a5
githubuser254 <96251809+githubuser254@users.noreply.github.com>
2022-01-07 08:58:50 -0500

    Delete python.py

--- python.py
+++
@@ -1,7 +0,0 @@
-import random
-
-passlength = int(input("Enter the Length of the password": "))
-char = "ajjdhhdsdnrnrnjdjjejnendnnenhfhjnhdnbebdhjkqk1i94958738ij"
-x = "".join(random.sample(char,passlength))
-                       
-print(x)
